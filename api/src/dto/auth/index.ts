cc4d8ead54504459f7f45a6d4883604cd6ce6677
raziyekavurgaci <raziyekavurgaci1234@gmail.com>
2025-07-27 23:13:35 +0300

    Added ts-node-dev to package.json and package-lock.json. Created a new middleware interceptor file. Updated the auth/controller.ts file.

---
+++ api/src/dto/auth/index.ts
@@ -0,0 +1,6 @@
+import * as loginDTO from "./login-dto";
+import * as registerDTO from "./register-dto";
+import * as verifyEmailDTO from "./verify-email.dto";
+import * as resetPasswordDTO from "./reset-password.dto";
+
+export { loginDTO, registerDTO, verifyEmailDTO, resetPasswordDTO };
