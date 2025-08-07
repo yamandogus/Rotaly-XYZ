7e416e430730318f6530e633472b32ee8e600f5e
caglayagmuricerr <caglayagmuricer@gmail.com>
2025-08-07 21:41:47 +0300

    added OTP field to password reset and verification email schemas; updated controller to handle OTP

--- api/src/dto/email/password-reset-email.dto.ts
+++ api/src/dto/email/password-reset-email.dto.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const passwordResetEmailSchema = z.object({
   email: z.string().email("Invalid email address"),
   name: z.string().min(1, "Name is required").max(100, "Name too long"),
+  otp: z.string(),
 });
 
 export type PasswordResetEmailSchemaType = z.infer<
--- api/src/dto/email/verification-email.dto.ts
+++ api/src/dto/email/verification-email.dto.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const verificationEmailSchema = z.object({
   email: z.string().email("Invalid email address"),
   name: z.string().min(1, "Name is required").max(100, "Name too long"),
+  otp: z.string(),
 });
 
 export type VerificationEmailSchemaType = z.infer<
