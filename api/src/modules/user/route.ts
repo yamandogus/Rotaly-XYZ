098743d9e668d08a838819aff46e1feb0efc4741
raziyekavurgaci <raziyekavurgaci1234@gmail.com>
2025-08-23 13:29:55 +0300

    refactor admin and user route

--- api/src/modules/user/route.ts
+++ api/src/modules/user/route.ts
@@ -1,28 +1,23 @@
 import { Router } from "express";
 import { UserController } from "./controller";
 import { authenticateToken } from "../../middleware/jwt.middleware";
-import { verifiedUser } from "../../middleware/auth.middleware";
 
 const router = Router();
 
 // Register a new user
 router.post("", UserController.add);
 // GET /api/users/me/profile
-router.get(
-  "/me/profile",
-  authenticateToken,
-  UserController.getProfile
-);
-router.get("/", authenticateToken, verifiedUser, UserController.index);
-// GET /api/users/:id
-router.get("/:id", authenticateToken, verifiedUser, UserController.ById);
-// GET /api/users/:email
-router.get("/:email", authenticateToken, verifiedUser, UserController.ByEmail);
-// GET /api/users/:phone
-router.get("/:phone", authenticateToken, verifiedUser, UserController.ByPhone);
+router.get("/me/profile", authenticateToken, UserController.getProfile);
+router.get("/", authenticateToken, UserController.index);
+// GET /api/users/id/:id
+router.get("/id/:id", authenticateToken, UserController.ById);
+// GET /api/users/email/:email
+router.get("/email/:email", authenticateToken, UserController.ByEmail);
+// GET /api/users/phone/:phone
+router.get("/phone/:phone", authenticateToken, UserController.ByPhone);
 // PUT /api/users/:id
-router.put("/:id", authenticateToken, verifiedUser, UserController.update);
+router.put("/:id", authenticateToken, UserController.update);
 // DELETE /api/users/:id
-router.delete("/:id", authenticateToken, verifiedUser, UserController.delete);
+router.delete("/:id", authenticateToken, UserController.delete);
 
 export default router;
